688dd4516f06748b93531407eaec0a5b62a2825b
Oliv4945 <oliv4945@gmail.com>
2018-03-19 23:37:43 +0100

    Software init

---
+++ software/mouflePower/src/define.h
@@ -0,0 +1,46 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#include <stdio.h>
+#include <Arduino.h>
+
+// I/O
+static const uint8_t TX        =  0;
+static const uint8_t RX        =  1;
+static const uint8_t RFM_INT0  =  2;
+static const uint8_t SW_FORCE  =  4;
+static const uint8_t EINK_BUSY =  5;
+static const uint8_t EINK_DC   =  6;
+static const uint8_t EINK_SS   =  7;
+static const uint8_t SPI_SS    =  9;
+static const uint8_t RFM_SS    = 10;
+static const uint8_t SPI_MOSI  = 11;
+static const uint8_t SPI_MISO  = 12;
+static const uint8_t SCLK      = 13;
+static const uint8_t ADC_MAIN  = A0;
+static const uint8_t ADC_AUX   = A1;
+static const uint8_t ADC_A2    = A2;
+static const uint8_t MOSFET    = A3;
+static const uint8_t I2C_SDA   = A4;
+static const uint8_t I2C_SCL   = A5;
+static const uint8_t ADC_A7    = A7;
+
+// Constants
+static const int EINK_BLACK = 0;
+static const int EINK_RED   = 1;
+
+// Debug activation
+#define SERIAL_BAUD 115200
+#ifdef SERIAL_BAUD 
+  #define DEBUG( input )    { Serial.print( input ); delay( 1 ); }
+  #define DEBUGDec( input ) { Serial.print( input, DEC ); delay( 1 ); }
+  #define DEBUGHex( input ) { Serial.print( input, HEX ); delay( 1 ); }
+  #define DEBUGln( input )  { Serial.println( input ); delay( 1 ); }
+#else
+  #define DEBUG( input )
+  #define DEBUGDec( input )
+  #define DEBUGHex( input )
+  #define DEBUGln( input )
+#endif
+
+#endif
---
+++ software/mouflePower/src/main.cpp
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include "define.h"
+#include "epd2in9b.h"
+#include "epdpaint.h"
+
+
+// Global objects
+Epd epd;
+
+void setup() {
+    // Pin initialisation
+    pinMode( MOSFET,   OUTPUT );
+    digitalWrite( MOSFET, LOW );
+    pinMode( RFM_INT0, INPUT );
+    pinMode( SW_FORCE, INPUT );
+    pinMode( SPI_SS,   OUTPUT );
+    digitalWrite( SPI_SS, HIGH );
+    pinMode( RFM_SS,   OUTPUT );
+    digitalWrite( RFM_SS, HIGH );
+    pinMode( ADC_MAIN, INPUT );
+    pinMode( ADC_AUX,  INPUT );
+    pinMode( ADC_A2,   INPUT );
+    pinMode( MOSFET,   OUTPUT );
+    digitalWrite( MOSFET, LOW );
+    pinMode( ADC_A7,   INPUT );
+
+    // Serial initialisation
+    #ifdef SERIAL_BAUD
+        Serial.begin( SERIAL_BAUD );
+    #endif
+    DEBUGln( "mouflePower - Starting" );
+
+    // E-Ink initialisation
+    if ( epd.Init( ) != 0 ) {
+        DEBUGln( "EINK - Failed init" );
+    } else {
+        epd.ClearFrame( );
+        
+        /**
+        * Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
+        * In this case, a smaller image buffer is allocated and you have to 
+        * update a partial display several times.
+        * 1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+        */
+        unsigned char image[1024];
+        Paint paint( image, 128, 18 );    //width should be the multiple of 8 
+
+        paint.Clear( EINK_BLACK );
+        paint.DrawStringAt( 0, 0, "mouflePower", &Font12, EINK_BLACK );
+        epd.SetPartialWindowRed( paint.GetImage( ), 24, 32, paint.GetWidth( ), paint.GetHeight( ) );
+        epd.DisplayFrame( );
+    }
+
+
+    
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
